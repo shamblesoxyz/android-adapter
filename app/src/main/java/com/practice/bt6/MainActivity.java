6805db0b6857842b1f8f7287eacfb3bca04b5e2d
Trong Phuc <phuc86119@gmail.com>
2024-03-08 16:55:48 +0700

    feat: simple listview

--- app/src/main/java/com/practice/bt6/MainActivity.java
+++ app/src/main/java/com/practice/bt6/MainActivity.java
@@ -3,12 +3,34 @@ package com.practice.bt6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtNewSubject;
+    Button btnAdd;
+    Button btnUpdate;
+    Button btnDelete;
+    ListView lstSubject;
+    ArrayList<String> arrSubject;
+    ArrayAdapter arrAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstSubject = (ListView) findViewById(R.id.listview_subject);
+        arrSubject = new ArrayList<>();
+        arrSubject.add("Java");
+        arrSubject.add("C#");
+        arrSubject.add("PHP");
+        arrAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrSubject);
+        lstSubject.setAdapter(arrAdapter);
     }
 }
