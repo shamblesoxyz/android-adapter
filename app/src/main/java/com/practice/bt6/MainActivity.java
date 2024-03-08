f7c636132575bd8f4edc8794cac418abd0a48b34
Trong Phuc <phuc86119@gmail.com>
2024-03-08 17:12:43 +0700

    feat: toast message when click and long click event for list item

--- app/src/main/java/com/practice/bt6/MainActivity.java
+++ app/src/main/java/com/practice/bt6/MainActivity.java
@@ -3,11 +3,14 @@ package com.practice.bt6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,12 +28,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Add data form ArrayList into ListView
         lstSubject = (ListView) findViewById(R.id.listview_subject);
         arrSubject = new ArrayList<>();
         arrSubject.add("Java");
         arrSubject.add("C#");
         arrSubject.add("PHP");
+        arrSubject.add("Kotlin");
+        arrSubject.add("Dart");
         arrAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arrSubject);
         lstSubject.setAdapter(arrAdapter);
+
+        //Show index of item is being clicked
+        lstSubject.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "" + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Show list item is being long clicked
+        lstSubject.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "" + i + " - " + arrSubject.get(i), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
